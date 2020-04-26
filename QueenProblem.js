a3940a08f785a9aaefa9a8db3c4c09ec73d0031f
yashasree <yashasreeaditya@gmail.com>
2020-04-26 10:42:18 +0530

    Few updates

--- QueenProblem.js
+++ QueenProblem.js
@@ -58,8 +58,6 @@ function updatePosition()
 }
 
 
-
-/*
 positionLog = 
 		  [["a8","b8","c8","d8","e8","f8","g8","h8"],
  		   ["a7","b7","c7","d7","e7","f7","g7","h7"],
@@ -69,7 +67,13 @@ positionLog =
            ["a3","b3","c3","d3","e3","f3","g3","h3"],
            ["a2","b2","c2","d2","e2","f2","g2","h2"],
            ["a1","b1","c1","d1","e1","f1","g1","h1"]];
-           
+
+
+
+
+
+
+/*           
 Console log an error message whenever the queen tries to step out of your 8x8 chessboard.
 
 Add more power to your queen by helping her move and jump move backward too!
